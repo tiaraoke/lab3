07307985ad6de2b5605412eb6ed1f22d6e17f8ae
tiaraoke <oktapianatiara@gmail.com>
2021-11-15 20:07:44 +0700

    tambahkan latihan2

---
+++ latihan2.py
@@ -0,0 +1,13 @@
+#latihan2
+
+a = input("masukan bilangan 1 = ")
+b = input("masukan bilangan 2 = ")
+c = input("masukan bilangan 3 = ")
+d = input("masukan bilangan 4 = ")
+e = input("masukan bilangan 5 = ")
+f = input("masukan bilangan 6 = ")
+g = input("masukan bilangan 7 = ")
+
+data = [a,b,c,d,e,f,g]
+
+print("Maka Bilangan terbesar adalah : ", max(data))
