61a43fc73830ab6bdaaf964b5cefd3dd82fec5c2
tiaraoke <oktapianatiara@gmail.com>
2021-11-15 20:03:01 +0700

    tambahkan latihan1

---
+++ latihan1.py
@@ -0,0 +1,14 @@
+#Latihan1
+
+print("masukan nilain n = 5")
+
+import random
+jumlah = 5
+a = 0 
+
+for x in range (jumlah):
+    i = random.uniform(.0,.5)
+    a+=1
+    print("data ke",a,"=",i)
+
+print("selesai")
